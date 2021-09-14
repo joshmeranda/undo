d464e7e3fdb2a27d199532befe96644058e91790
joshmeranda <joshmeranda@gmail.com>
2021-09-13 20:05:31 -0400

    properlly handle empty groups and ANY ArgNums

--- undo/pattern/__init__.py
+++ undo/pattern/__init__.py
@@ -3,65 +3,6 @@ from .pattern import *
 import argparse
 
 
-# def __add_arg_to_parser(parser, arg: ArgumentPattern, positional_count: int):
-#     """Add an ArgumentPattern to the parser.
-# 
-#     :parser: the target parser.
-#     :param arg: the argument pattern to add to the parser.
-#     :param positional_count: the next number for a positional argument with n given name.
-#     """
-#     if arg.is_positional:
-#         if arg.var_name is not None:
-#             names = [arg.var_name]
-#         else:
-#             names = str(positional_count)
-# 
-#         positional_count += 1
-#     else:
-#         names = arg.args
-# 
-#     kwargs = dict()
-# 
-#     if arg.arg_num.quantifier == Quantifier.ANY:
-#         kwargs["nargs"] = "*"
-# 
-#         if arg.delim is not None and arg.delim != " ":
-#             kwargs["nargs"] = "?"
-#             kwargs["const"] = []
-#             kwargs["type"] = lambda s: [i for i in s.split(arg.delim) if i]
-#     elif arg.arg_num.quantifier == Quantifier.AT_LEAST_ONE:
-#         kwargs["nargs"] = "+"
-# 
-#         if arg.delim is not None:
-#             kwargs["nargs"] = None
-#             kwargs["type"] = lambda s: [i for i in s.split(arg.delim) if i]
-#     elif arg.arg_num.count == 0:
-#         kwargs["action"] = "store_true"
-#     elif arg.arg_num.count == 1:
-#         kwargs["nargs"] = None
-#     elif arg.arg_num.count > 1:
-#         kwargs["nargs"] = arg.arg_num.count
-# 
-#     if not arg.is_positional:
-#         kwargs["required"] = arg.is_required
-#         kwargs["dest"] = arg.var_name
-# 
-#     parser.add_argument(*names, **kwargs)
-
-
-# def __add_group_to_parser(parser, group: ArgumentGroupPattern, positional_count: int):
-#     """Add an ArgumentGroupPattern to the parser.
-# 
-#     :param parser: the target parser.
-#     :param group: the ArgumentGroupPattern to add to the parser.
-#     :param positional_count: the next number for a positional argument with n given name.
-#     """
-#     group_parser = parser.add_mutually_exclusive_group(required=group.is_required)
-# 
-#     for arg in group.args:
-#         __add_arg_to_parser(group_parser, arg, positional_count)
-
-
 class _UndoArgumentParser(argparse.ArgumentParser):
     """Simple child class of ArgumentParser designed to allow for handling argument parsing error."""
 
@@ -124,24 +65,26 @@ class _UndoArgumentParser(argparse.ArgumentParser):
             self.__required_groups.append([arg for arg in group.args])
 
         for arg in group.args:
-            if arg.is_required:
-                self.error(f"")
             self.add_argument_pattern(arg)
 
     def parse_args(self, args=None, namespace=None):
         namespace = super().parse_args(args, namespace)
+
+        items = vars(namespace)
 
         # ensure that at least one of the required group arguments are present
         for group in self.__required_groups:
             group_found = False
 
             for arg in group:
-                if arg.var_name in vars(namespace) and vars(namespace)[arg.var_name]:
+                # must explicitly check if items[arg.var_name] since an empty list is considered found but also "Falsey"
+                if arg.var_name in items and items[arg.var_name] not in (None, False):
+                    value = items[arg.var_name]
 
-                    group_found = True
-                    break
+                    if value is not None and value is not False:
+                        group_found = True
 
-            if not group_found:
+            if not group_found and len(group) > 0:
                 self.error(f"expected at least one of {' '.join(name for arg in group for name in arg.args)}")
 
         return namespace
