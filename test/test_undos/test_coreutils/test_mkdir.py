a9ef39ad8b88d51c57e21f103cbd8f441c3a9604
joshmeranda <joshmeranda@gmail.com>
2021-09-23 22:00:06 -0400

    implement undo for coreutil mkdir

---
+++ test/test_undos/test_coreutils/test_mkdir.py
@@ -0,0 +1,30 @@
+import unittest
+
+from test.test_undos.test_coreutils import common
+from undo import expand, resolve
+
+
+class MyTestCase(unittest.TestCase):
+    def test_mkdir_single(self):
+        command = "mkdir A"
+
+        expected = ["rm A"]
+        actual = [expand.expand(undo, env, ("%", "%"), "; ")
+                  for env, undo in
+                  resolve.resolve(command, [common.COREUTILS_UNDO_DIR], False, False, "sh")]
+
+        self.assertListEqual(expected, actual)
+
+    def test_mkdir_multiple(self):
+        command = "mkdir A B C"
+
+        expected = ["rm A B C"]
+        actual = [expand.expand(undo, env, ("%", "%"), "; ")
+                  for env, undo in
+                  resolve.resolve(command, [common.COREUTILS_UNDO_DIR], False, False, "sh")]
+
+        self.assertListEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
