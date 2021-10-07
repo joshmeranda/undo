2592005cd718da8a74f8e427136a418aacccf389
joshmeranda <joshmeranda@gmail.com>
2021-10-06 23:43:06 -0400

    cleaner history reading and stream documentation

--- undo/history.py
+++ undo/history.py
@@ -18,33 +18,27 @@ def __read_commands(stream: typing.TextIO, n: int, func: typing.Callable[[str], 
                     stream.readlines()[-(n + 1):-1]))
 
 
-def __generic_history(cmd: list[str], limit: int, file: typing.Optional[typing.TextIO],
+def __generic_history(cmd: list[str], limit: int, stream: typing.Optional[typing.TextIO],
                       func: typing.Callable[[str], str] = lambda line: line) -> list[str]:
     """Provides a wrapper around a shell history parser function.
 
-    todo: remove file in favor of cmd = ['cat', 'path/to/file']
-
     :param cmd: the command to call to retrieve the command history.
     :param limit: the maximum amount off history entries too return.
-    :param file: The file-like object to read history data from.
+    :param stream: The file-like object to read history data from.
     :param func: the history parsing function, defaults to a simple pass-through.
     """
-    if file is None:
+    if stream is None:
         logging.debug(f"running history command '{' '.join(cmd)}'")
         proc = subprocess.run(cmd, capture_output=True)
         stream = io.StringIO(proc.stdout.decode("utf-8"))
-    else:
-        stream = file
 
-    commands = __read_commands(stream, limit, func)
+        return __read_commands(stream, limit, func)
 
-    if file is None:
-        stream.close()
-
-    return commands
+    with stream:
+        return __read_commands(stream, limit, func)
 
 
-def __history_sh(path: str, limit: int, file: typing.Optional[typing.TextIO]) -> list[str]:
+def __history_sh(path: str, limit: int, stream: typing.Optional[typing.TextIO]) -> list[str]:
     def parse_sh_history(line: str) -> str:
         match = re.match("  [1-9][0-9]*  (.*)", line)
 
@@ -56,15 +50,15 @@ def __history_sh(path: str, limit: int, file: typing.Optional[typing.TextIO]) ->
     return __generic_history(
         cmd=[path, "-c", f"history {limit + 1}"],
         limit=limit,
-        file=file,
+        stream=stream,
         func=parse_sh_history)
 
 
-def __history_fish(path: str, limit: int, file: typing.Optional[typing.TextIO]) -> list[str]:
+def __history_fish(path: str, limit: int, stream: typing.Optional[typing.TextIO]) -> list[str]:
     return __generic_history(
         cmd=[path, "--command", f"history --reverse --max {limit + 1}"],
         limit=limit,
-        file=file)
+        stream=stream)
 
 
 def history(shell: str, limit: int = 1, stream: typing.Optional[typing.TextIO] = None) -> list[str]:
@@ -75,7 +69,12 @@ def history(shell: str, limit: int = 1, stream: typing.Optional[typing.TextIO] =
 
     If `shell` is not specified, the process name of the ppid is used (/proc/<ppid>/comm).
 
-    If `file` is not specified, the default file paths for the shell is used.
+    If `stream` is provided, the given shell's history command is ignored, and the command history is read from stream
+    instead; however, the `shell` argument is still needed to specify the history format. It is important to note that
+    Undo parses according to the history command output, not the hisstory file format. So in the case of shells like
+    fish, whose history command and history file formats are entirely different, to produce a stream a command like
+    `cat $HOME/.local/share/fish/fish_history` would not be sufficient, but a command like `history . out & cat out`
+    would produce a usable stream.
 
     Note that the commands returned by this method will be those returned by the given (or determined) shell builtin
     history command.
